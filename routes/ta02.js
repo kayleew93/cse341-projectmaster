8475ab8a171e7a53bb0b72a353eb4cc4bbd74691
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2022-01-12 18:01:19 -0700

    Team 02

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,74 @@
 const express = require('express');
 const router = express.Router();
 
+const alert = require('alert');
+
+let message = "";
+
+// me
+const names = ['John', 'Bob', 'Bill'];
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02.ejs', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    names: names,
+    errorMessage: message
   });
+});
+
+router.post('/addUser', (req, res, next) => {
+  // me
+  const newUser = req.body.addUser
+
+  function containsObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+
+    return false;
+}
+  if ( containsObject(newUser, names) ) {
+    alert("Error: Duplicate Entry");
+    message = "Error: Duplicate Entry";
+  } else {
+    names.push(newUser);
+  };
+
+  
+
+  res.render('pages/ta02', {
+    title: 'Add User',
+    path: '',
+    errorMessage: message
+  })
+
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const removeUser = req.body.removeUser;
+  const index = names.indexOf(removeUser);
+  if (index === -1) {
+    alert("Error: No user of that name to remove");
+    message = "Error: No users of that name";
+  }
+  if (index > -1) {
+    names.splice(index, 1)
+  }
+
+  res.render('pages/ta02', {
+    title: 'Add User',
+    path: '',
+    errorMessage: message
+  })
+
+  res.redirect('/ta02');
 });
 
 module.exports = router;
